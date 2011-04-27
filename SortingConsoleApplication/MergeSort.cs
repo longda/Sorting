d10322e1b72256447f6a3113e8839ec6a7ec3587
David Long <longda@gmail.com>
2011-04-26 19:58:18 -0700

    initial commit

---
+++ SortingConsoleApplication/MergeSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortingConsoleApplication
+{
+    public static class MergeSort
+    {
+        public static int[] Sort(int[] input)
+        {
+            if (input == null || input.Length <= 1)
+                return input;
+
+            int middle = input.Length / 2;
+
+            int[] left = Sort(input.Take(middle).ToArray());
+            int[] right = Sort(input.Skip(middle).Take(input.Count() - middle).ToArray());
+
+            return Merge(left, right);
+        }
+
+        private static int[] Merge(int[] left, int[] right)
+        {
+            List<int> output = new List<int>();
+            List<int> l = new List<int>(left);
+            List<int> r = new List<int>(right);
+
+            while (l.Count() > 0 || r.Count() > 0)
+            {
+                if (l.Count() > 0 && r.Count() > 0)
+                {
+                    if (l.First<int>() <= r.First<int>())
+                    {
+                        output.Add(l.First<int>());
+                        l.RemoveAt(0);
+                    }
+                    else
+                    {
+                        output.Add(r.First<int>());
+                        r.RemoveAt(0);
+                    }
+                }
+                else if (l.Count() > 0)
+                {
+                    output.Add(l.First<int>());
+                    l.RemoveAt(0);
+                }
+                else if (r.Count() > 0)
+                {
+                    output.Add(r.First<int>());
+                    r.RemoveAt(0);
+                }
+            }
+
+            return output.ToArray();
+        }
+    }
+}
---
+++ SortingConsoleApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortingConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            while (input.ToLower() != "exit")
+            {
+                Random r = new Random();
+                List<int> listToSort = new List<int>();
+                Console.WriteLine("Generating list...");
+
+                for (int i = 0; i < r.Next(20); i++)
+                {
+                    listToSort.Add(r.Next(100));
+                }
+
+                PrintList(listToSort);
+
+                Console.WriteLine("");
+                Console.WriteLine("Sorting list...");
+
+                listToSort = new List<int>(MergeSort.Sort(listToSort.ToArray()));
+
+                PrintList(listToSort);
+
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to run again. Type 'exit' to end program.");
+                Console.WriteLine("");
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void PrintList(List<int> input)
+        {
+            Console.WriteLine("List Contents:");
+            input.ForEach(a => Console.Write("{0} ", a.ToString()));
+            Console.WriteLine("");
+        }
+    }
+}
